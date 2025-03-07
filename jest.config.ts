972d6e47a12275f30ac3397378689b9aa9c2ce25
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-03-07 12:01:06 +0400

    fix: ensure React is correctly imported in Jest environment

--- jest.config.ts
+++ jest.config.ts
@@ -4,12 +4,12 @@ const config: Config = {
     verbose: true,
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
-        '\\.svg$': '<rootDir>/__mocks__/svgMock.ts',
     },
     transform: {
         '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
     },
     testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
 }
 
 export default config
