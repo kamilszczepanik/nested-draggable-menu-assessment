a11cadc7bcff56503712f0b0aaf0632f7027e609
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-03-07 12:01:14 +0400

    refactor: deleted comments

--- src/tests/integration/Menu/AddMenuItemForm.test.tsx
+++ src/tests/integration/Menu/AddMenuItemForm.test.tsx
@@ -1,9 +1,3 @@
-// Should render the form with default values when no editingItem is provided.
-// Should populate form fields with editingItem values if provided.
-// Should validate required fields and show errors.
-// Should call addMenuItem or editMenuItem on form submission.
-// Should close the form when the "Cancel" button is clicked.
-
 import AddMenuItemForm from '@/components/Menu/AddMenuItemForm'
 import { IFormState } from '@/types/form'
 import { render, screen } from '@testing-library/react'
