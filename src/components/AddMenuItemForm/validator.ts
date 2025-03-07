f4084df19cf4c26a0a17ff96638bb5aa6bebb3b5
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-03-07 12:00:20 +0400

    feat(ui): add basic sublinks tree, rebuild AddMenuItemForm component, add validation

---
+++ src/components/AddMenuItemForm/validator.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const AddMenuItemFormSchema=z.object({
+    name: z.string(),
+    url: z.string().url().optional(),
+})
+
+export type AddMenuFormFields=z.infer<typeof AddMenuItemFormSchema>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,28 @@
-import AddMenuItemForm from "@/components/AddMenuItemForm";
+import AddMenuItemForm from "@/components/AddMenuItemForm/AddMenuItemForm";
 import EmptyMenu from "@/components/EmptyMenu";
+import { IMenuFormFields, MenuItem } from "@/components/MenuItem/MenuItem";
 import { useState } from "react";
 
 export default function Home() {
+  const [menuItems,] = useState<IMenuFormFields[]>([{
+    name: 'test',
+    url: '1',
+    subLinks: [
+      {name: 'test 1', url: 't', subLinks: [{name: 'test 1 1'},{name: 'test 1 2', subLinks: [{name: 'test 1 1'},{name: 'test 1 2',subLinks: [{name: 'test 1 2 1'}]}]}
+    ]
+  }]}])
   const [showAddMenuItemForm, setShowAddMenuItemForm] = useState(false);
 
   return (
     <div
       className={`grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
+        {menuItems ? <MenuItem menuItems={menuItems} /> :  <EmptyMenu setShowAddMenuItemForm={setShowAddMenuItemForm} />}
         <EmptyMenu setShowAddMenuItemForm={setShowAddMenuItemForm} />
-        <AddMenuItemForm
-          showAddMenuItemForm={showAddMenuItemForm}
+        {showAddMenuItemForm ? <AddMenuItemForm
           setShowAddMenuItemForm={setShowAddMenuItemForm}
-        />
+        /> : null}
       </main>
     </div>
   );
