cfb989cf99400ca438f9dbbaa6c24098a338bf47
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-03-07 12:01:08 +0400

    test: add unit tests for EmptyMenu component

--- src/tests/Menu/EmptyMenu.test.tsx
+++ src/tests/Menu/EmptyMenu.test.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react'
+import React from 'react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import EmptyMenu from '@/components/Menu/EmptyMenu'
 
@@ -6,6 +7,27 @@ const mockOpenForm = jest.fn()
 
 test('Should render an empty state message', async () => {
     render(<EmptyMenu openForm={mockOpenForm} />)
+
+    expect(
+        screen.getByRole('heading', { name: /Menu jest puste/i })
+    ).toBeVisible()
+    expect(
+        screen.getByText('W tym menu nie ma jeszcze żadnych linków.')
+    ).toBeVisible()
+    expect(
+        screen.getByRole('button', {
+            name: /Dodaj pozycję menu/i,
+        })
+    ).toBeVisible()
 })
 
-test('Should call openForm when "Add Menu Item" is clicked', async () => {})
+test('Should call openForm when "Add Menu Item" is clicked', async () => {
+    render(<EmptyMenu openForm={mockOpenForm} />)
+
+    screen.getByRole('button', { name: /Dodaj pozycję menu/i }).click()
+
+    expect(mockOpenForm).toHaveBeenCalledWith({
+        parentId: null,
+        editingId: null,
+    })
+})
