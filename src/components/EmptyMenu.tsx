cffb38724adbcab756e30c0a5da3c1327b045019
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-03-07 12:00:11 +0400

    feat(ui): add logic to toggle menu item form visibility

--- src/components/EmptyMenu.tsx
+++ src/components/EmptyMenu.tsx
@@ -1,4 +1,8 @@
-const EmptyMenu = () => {
+interface Props {
+  setShowAddMenuItemForm: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const EmptyMenu = ({setShowAddMenuItemForm}: Props) => {
   return (
     <div className="flex flex-col items-center justify-center text-center gap-6 py-10">
       <div>
@@ -7,7 +11,9 @@ const EmptyMenu = () => {
           W tym menu nie ma jeszcze żadnych linków.
         </p>
       </div>
-      <button className="flex items-center justify-center px-4 py-2 bg-purple-700 text-white rounded-md shadow hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-400">
+      <button className="flex items-center justify-center px-4 py-2 bg-purple-700 text-white rounded-md shadow hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-400" 
+      onClick={() => {setShowAddMenuItemForm(true)}}
+      >
         Dodaj pozycję menu
       </button>
     </div>
