bbd1ebbe44a6f3e2027c82a89a82b74ac8b821f4
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-03-07 12:00:14 +0400

    feat(ui): making buttons reusable

---
+++ src/components/Buttons/ButtonPrimary.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+interface IButtonPrimaryProps {
+  text?: string,
+  onClick?: () => void,
+}
+
+
+const ButtonPrimary = ({text, onClick}: IButtonPrimaryProps) => {
+  return (
+    <button
+    type="submit"
+    onClick={onClick}
+    className="px-4 py-2 bg-white text-[#344054] rounded-md shadow hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-400"
+  >
+    {text}
+  </button>
+  )
+}
+
+export default ButtonPrimary
